5125b7824da51f2f7c7b91944e2be598dc4126ee
Edward0181 <edwardroggeveen@gmail.com>
2022-09-07 10:23:33 +0200

    several updates

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=/home/edward/toolchains/proton-clang-13-clang/bin/aarch64-linux-gnu-
+export CC=/home/edward/toolchains/proton-clang-13-clang/bin/clang
+export CLANG_TRIPLE=/home/edward/toolchains/proton-clang-13-clang/bin/aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=s
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y edward0181_defconfig LLVM=1
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16 LLVM=1
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y edward0181_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
